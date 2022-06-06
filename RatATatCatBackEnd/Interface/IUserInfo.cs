99de51e5eca4c0e84000687a5483a6264c35157c
s459315 <alebur2@st.amu.edu.pl>
2022-06-06 18:05:28 +0200

    Added aws db connection and basic api for users

---
+++ RatATatCatBackEnd/Interface/IUserInfo.cs
@@ -0,0 +1,14 @@
+﻿using RatATatCatBackEnd.Models;
+
+namespace RatATatCatBackEnd.Interface
+{
+    public interface IUserInfo
+    {
+        public List<UserInfo> GetUsers();
+        public UserInfo GetUserInfo(int id);
+        public void AddUser(UserInfo user);
+        public void UpdateUser(UserInfo user);
+        public UserInfo DeleteUser(int id);
+        
+    }
+}
--- RatATatCatBackEnd/Program.cs
+++ RatATatCatBackEnd/Program.cs
@@ -1,3 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+using RatATatCatBackEnd.Interface;
+using RatATatCatBackEnd.Models;
+using RatATatCatBackEnd.Repository;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<DatabaseContext>
+    (options => options.UseSqlServer(builder.Configuration.GetConnectionString("dbConnection")));
+builder.Services.AddTransient<IUserInfo, UserInfoRepository>();
 
 var app = builder.Build();
 
