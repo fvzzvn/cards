8a5daa4645b13324b140e9ed0f258857c2820d85
s459315 <alebur2@st.amu.edu.pl>
2022-06-07 17:28:06 +0200

    configured db connection and program.cs file

--- RatATatCatBackEnd/Program.cs
+++ RatATatCatBackEnd/Program.cs
@@ -1,3 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+using RatATatCatBackEnd.Interface;
+using RatATatCatBackEnd.Models;
+using RatATatCatBackEnd.Repository;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,7 +12,12 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<DatabaseContext>
+    (options => options.UseSqlServer(builder.Configuration.GetConnectionString("dbConnection")));
+builder.Services.AddTransient<IBoardInstance, BoardInstanceRepository>();
+
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
