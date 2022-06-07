f312b0d0082a0773dec8c2c1c9a802436694a05c
s459315 <alebur2@st.amu.edu.pl>
2022-06-07 17:30:12 +0200

    Created model for boardInstance, created a class that connects with database

---
+++ RatATatCatBackEnd/Interface/IBoardInstance.cs
@@ -0,0 +1,14 @@
+﻿using RatATatCatBackEnd.Models;
+
+namespace RatATatCatBackEnd.Interface
+{
+    public interface IBoardInstance
+    {
+        public List<BoardInstance> GetBoards();
+        public BoardInstance GetBoard(int id);
+        public void AddBoard(BoardInstance board);
+        public BoardInstance RemoveBoard(int id);
+        public void UpdateBoard(BoardInstance board);
+        public bool CheckBoard(int id);
+    }
+}
---
+++ RatATatCatBackEnd/Models/DatabaseContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace RatATatCatBackEnd.Models
+{
+    public partial class DatabaseContext : DbContext
+    {
+        public DatabaseContext()
+        {
+        }
+
+        public DatabaseContext(DbContextOptions<DatabaseContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<BoardInstance>? BoardInstances { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<BoardInstance>(entity =>
+            {
+                //entity.HasNoKey();
+                entity.ToTable("Boards");
+                entity.Property(e => e.Id).HasColumnName("Id"); ;
+                entity.Property(e => e.BoardType);
+                entity.Property(e => e.BoardMode);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
