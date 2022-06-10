9620aa65a0dd8401ef58d47598d66e10e9b675ce
s459315 <alebur2@st.amu.edu.pl>
2022-06-10 23:15:36 +0200

    Changed to models added methods and logic for gameplay

--- RatATatCatBackEnd/Hubs/GameHub.cs
+++ RatATatCatBackEnd/Hubs/GameHub.cs
@@ -6,25 +6,87 @@ namespace RatATatCatBackEnd.Hubs
 {
     public class GameHub : Hub<IGameHub>
     {
-        public async Task JoinRoom(string roomId, string username)
+        public async Task JoinRoom(string gameId, string username)
         {
-            if(GameState.Instance.GetGame(roomId) == null)
+            if (GameState.Instance.GetGame(gameId) == null)
             {
-                GameState.Instance.CreateGame(roomId);
+                await GameState.Instance.CreateGame(gameId);
             }
 
-            Player player = 
-                GameState.Instance.CreatePlayer(roomId, username, Context.ConnectionId);
-            this.Clients.All.playerJoined(player);
+            Player player =
+                GameState.Instance.CreatePlayer(gameId, username, Context.ConnectionId);
+            await Clients.Caller.playerJoined(player);
 
-            await Groups.AddToGroupAsync(Context.ConnectionId, roomId);
+            await Groups.AddToGroupAsync(Context.ConnectionId, gameId);
 
-            if (GameState.Instance.IsPlayersReady(roomId))
+            if (GameState.Instance.IsPlayersReady(gameId))
             {
-                Game game = GameState.Instance.GetGame(roomId);
-                this.Clients.All.start(game);
+                Game game = GameState.Instance.GetGame(gameId);
+                await Clients.All.start(game);
             }
 
+        }
+
+        public async Task PlayCard(Card card)
+        {
+            Player player = GameState.Instance.GetPlayer(Context.ConnectionId);
+            Game game = GameState.Instance.GetGame(player.GameId);
+
+            game.PlayCard(card, player);
+
+            await Clients.All.playerPlayedCard(player, card, game);
+        }
+        public async Task PlayCardAfterGet(Card card)
+        {
+            Player player = GameState.Instance.GetPlayer(Context.ConnectionId);
+            Game game = GameState.Instance.GetGame(player.GameId);
+
+            game.PlayCardAfterGet(card, player);
+
+            await Clients.All.playerPlayedCard(player, card, game);
+        }
+        public async Task GetCard(string from)
+        {
+            Player player = GameState.Instance.GetPlayer(Context.ConnectionId);
+            Game game = GameState.Instance.GetGame(player.GameId);
+
+            Card card = new Card();
+
+            if (game.PlayerTurn == player)
+            {
+                if (from == "dealer")
+                {
+                    game.Dealer.GiveCard(player);
+                    await Clients.All.playerTookCard(player, card, from);
+                    game.NextTurn();
+                }
+                else if (from == "stack")
+                {
+                    if (game.Stack.NotEmpty())
+                    {
+                        card = game.GetCardFromStack(player);
+                        await Clients.All.playerTookCard(player, card, from);
+                        game.NextTurn();
+                    }
+                    else
+                    {
+                        await Clients.Caller.stackEmpty();
+                    }
+                }
+            }
+            else
+            {
+                await Clients.Caller.notPlayersTurn();
+            }
+        }
+        public async Task EndGame(Player player)
+        {
+            Game game = GameState.Instance.GetGame(player.GameId);
+
+            /* Todo
+                game.End();
+            */
+            await Clients.All.gameEnding();
         }
         public Task LeaveRoom(string roomId)
         {
