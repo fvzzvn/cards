f07af048325476010fc21847b1dcad640f80547d
Piotr Dębski <piodeb1@st.amu.edu.pl>
2022-06-13 08:42:22 +0200

    buttons added, some animations

--- frontend/src/components/Boards.js
+++ frontend/src/components/Boards.js
@@ -1,10 +1,17 @@
 import Board from "./Board.js";
+import Button from "react-bootstrap/Button";
+import ToggleButton from "react-bootstrap/ToggleButton";
 
 const Boards = () => {
   return (
     <div className="home-boards-bg">
       <div className="home-boards-title">stoły</div>
       <div className="home-boards-container">
+        <ToggleButton variant="outline-primary" className="home-board-fliter filter">Rankingowe</ToggleButton>
+        <ToggleButton variant="outline-primary" className="home-board-fliter filter">Nierankingowe</ToggleButton>
+        <ToggleButton variant="outline-primary" className="home-board-fliter filter">Wolne miejsca</ToggleButton>
+        <ToggleButton variant="outline-primary" className="home-board-fliter filter">Rozpoczęte</ToggleButton>
+        <Button variant="secondary" className="home-board-fliter">Stwórz nową grę</Button>
         {[...Array(17)].map((e, i) => (
           <Board key={i}> </Board>
         ))}
