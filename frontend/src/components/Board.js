310f929c29eb312c9cf48d4494e8e307f1162364
Piotr Dębski <piodeb1@st.amu.edu.pl>
2022-06-13 07:08:18 +0200

    Boards and Board component added, basic scss

--- frontend/src/components/Board.js
+++ frontend/src/components/Board.js
@@ -1,9 +1,23 @@
 const Board = () => {
-    return(
-        <div className="board-card">
-            
-        </div>
-    )
-}
+  return (
+    <div className="board-card">
+      <div className="board-card-id">
+        <div className="card-id">101</div>
+      </div>
+      <div className="board-card-ranking">
+        <div className="card-ranking">1386</div>
+      </div>
+      <div className="board-card-name">
+        <div className="card-name">Nazwa gry</div>
+      </div>
+      <div className="board-card-players">
+        <div className="player-1">player1</div>
+        <div className="player-2">player2</div>
+        <div className="player-3">player3</div>
+        <div className="player-4">-</div>
+      </div>
+    </div>
+  );
+};
 
-export default Board
+export default Board;
