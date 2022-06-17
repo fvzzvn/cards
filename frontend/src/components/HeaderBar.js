292ac223089a59f591ca23a2bc5f6bd45610c263
Piotr Dębski <piodeb1@st.amu.edu.pl>
2022-06-17 04:48:10 +0200

    logout button

--- frontend/src/components/HeaderBar.js
+++ frontend/src/components/HeaderBar.js
@@ -2,11 +2,15 @@ import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { clearMessage } from "../slices/message";
 import { getUserCredentials } from "../slices/userCredentials";
+import { logout } from "../slices/auth";
+import { useNavigate } from "react-router-dom";
+import Button from "react-bootstrap/Button";
 
 const HeaderBar = () => {
   const dispatch = useDispatch();
   const { username } = useSelector((state) => state.userCredentials.userName);
   const [loading, setLoading] = useState(false);
+  const history = useNavigate();
 
   useEffect(() => {
     dispatch(clearMessage());
@@ -21,13 +25,20 @@ const HeaderBar = () => {
       });
   }, [dispatch]);
 
+  const handleLogOut = () => {
+    dispatch(logout()).then(history("/"));
+  };
+
   return (
     <div className="header-bar-container">
+      <div className="logout-wrapper">
+        <Button onClick={handleLogOut}>LOGOUT</Button>
+      </div>
       <div className="header-bar-logo"></div>
       {loading ? (
         <span className="board-loader spinner-border spinner-border-sm"></span>
       ) : (
-        <div> {username}</div>
+        <div style={{ color: "white", fontSize: "40px" }}> {username}</div>
       )}
     </div>
   );
