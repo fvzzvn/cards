9bf776b707efc839c8d6ea374070bf4491f5bae6
s459315 <s459315@st.amu.edu.pl>
2022-06-18 16:02:39 +0200

    fixed a typo

--- RatATatCatBackEnd/Interface/IGameHub.cs
+++ RatATatCatBackEnd/Interface/IGameHub.cs
@@ -12,6 +12,6 @@ namespace RatATatCatBackEnd.Interfaces
         Task stackEmpty();
         Task notPlayersTurn();
         Task gameEnding();
-        Task RecieveMessage(string username,string message);
+        Task recieveMessage(string username,string message);
     }
 }
