6684f822935d3ae6371ce8a0c3bb38ad64ea7358
Paweł Radny <pawrad@st.amu.edu.pl>
2022-11-09 14:36:26 +0100

    Added UserImage Endpoint

---
+++ RatATatCatBackEnd/Controllers/UserImageController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RatATatCatBackEnd.Models.APIModels;
+
+namespace RatATatCatBackEnd.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserImageController : ControllerBase
+    {
+        private readonly IWebHostEnvironment _env;
+
+        public UserImageController(IWebHostEnvironment env)
+        {
+            _env = env;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(IFormFile image)
+        {
+            try
+            {
+                if (image.Length > 0)
+                {
+                    string imagePath = _env.WebRootPath + "\\UserImages";
+                    if (!Directory.Exists(imagePath))
+                    {
+                        Directory.CreateDirectory(imagePath);
+                    }
+
+                    using (FileStream fileStream = System.IO.File.Create(imagePath + "\\"+ image.FileName))
+                    {
+                        await image.CopyToAsync(fileStream);
+                        await fileStream.FlushAsync();
+                    }
+                }
+            } catch (Exception ex)
+            {
+                throw ex;
+            }
+            return Ok();
+        }
+    }
+}
---
+++ RatATatCatBackEnd/Models/APIModels/UserImage.cs
@@ -0,0 +1,9 @@
+﻿namespace RatATatCatBackEnd.Models.APIModels
+{
+    public class UserImage
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string ImagePath { get; set; }
+    }
+}
