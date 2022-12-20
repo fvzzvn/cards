ebe29a35b45bec454e2daef6922e8ab1f42f5308
Paweł Radny <pawrad@st.amu.edu.pl>
2022-12-20 13:41:40 +0100

    Created a new Hub for CrowGame

---
+++ RatATatCatBackEnd/Interface/ICrowGameHub.cs
@@ -0,0 +1,17 @@
+﻿using RatATatCatBackEnd.Models.GameModels;
+
+namespace RatATatCatBackEnd.Interface
+{
+    public interface ICrowGameHub
+    {
+        Task playerJoined(Player player);
+        Task start(IGame game);
+        Task playerPlayedCard(Player player, Card card, IGame game);
+        Task playerPlayedSpecialCard(Player player, Card card, IGame game);
+        Task applySpecialCardEffect(Card card, Player players, IGame cards);
+        Task notPlayersTurn();
+        Task nextTurn(IGame game);
+        Task gameResults(Dictionary<Player, int> gameResults);
+        Task receiveMessage(string username, string message);
+    }
+}
