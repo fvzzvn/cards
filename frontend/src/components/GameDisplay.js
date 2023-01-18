7b61e6f7f5674a0ab467ef8a8d3784d3c3b82517
Piotr Dębski <piodeb1@st.amu.edu.pl>
2023-01-18 23:46:37 +0100

    api link changed

---
+++ frontend/src/components/GameDisplay.js
@@ -0,0 +1,15 @@
+
+
+const GameDisplay = (props) => {
+
+    console.log(props);
+    const bottom = props.bottom;
+    const left = props.left;
+    const top = props.top;
+    const right = props.right;
+
+
+    
+}
+
+export default GameDisplay 
